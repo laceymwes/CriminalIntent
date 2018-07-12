8e73c80130936b0399f55d602ede878a951db3a9
Michael Lacey <laceymwes@gmail.com>
2018-07-12 06:06:19 -0600

    - Add options menu to CrimePagerActivity. - User now has the option to add Crime object.

--- app/src/main/java/edu/cnm/deepdive/criminalintent/controller/CrimeListFragment.java
+++ app/src/main/java/edu/cnm/deepdive/criminalintent/controller/CrimeListFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -31,7 +34,7 @@ public class CrimeListFragment extends Fragment {
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    mCrime = new Crime();
+    setHasOptionsMenu(true);
   }
 
   @Nullable
@@ -65,6 +68,26 @@ public class CrimeListFragment extends Fragment {
   public void onResume() {
     super.onResume();
     updateUI();
+  }
+
+  @Override
+  public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    super.onCreateOptionsMenu(menu, inflater);
+    inflater.inflate(R.menu.fragment_crime_list, menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.new_crime:
+        Crime crime = new Crime();
+        CrimeLab.get(getActivity()).addCrime(crime);
+        Intent intent = CrimePagerActivity.newIntent(getActivity(), crime.getId());
+        startActivity(intent);
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
   }
 
   private class CrimeHolder extends RecyclerView.ViewHolder {
