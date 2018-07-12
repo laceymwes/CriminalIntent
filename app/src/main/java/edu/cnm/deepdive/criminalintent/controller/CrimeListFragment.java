b59af67fc1c55755bdde520a26b97aa033aea379
Michael Lacey <laceymwes@gmail.com>
2018-07-12 12:42:50 -0600

    - Add upward navigation from CrimeFragment. - Add toolbar menu and options.      - crime creation action item.      - show/hide subtitle (num crimes) action item.

--- app/src/main/java/edu/cnm/deepdive/criminalintent/controller/CrimeListFragment.java
+++ app/src/main/java/edu/cnm/deepdive/criminalintent/controller/CrimeListFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -24,12 +25,15 @@ import java.util.List;
 
 public class CrimeListFragment extends Fragment {
 
+  private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
+
   private Crime mCrime;
   private EditText mTitleField;
   private Button mDateButton;
   private CheckBox mSolvedCheckBox;
   private RecyclerView mCrimeRecyclerView;
   private CrimeAdapter mCrimeAdapter;
+  private boolean mSubtitleVisible;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,6 +52,9 @@ public class CrimeListFragment extends Fragment {
 
     updateUI();
 
+    if (savedInstanceState != null) {
+      mSubtitleVisible = savedInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
+    }
     return view;
   }
 
@@ -62,6 +69,7 @@ public class CrimeListFragment extends Fragment {
       mCrimeAdapter.notifyDataSetChanged();
     }
 
+    updateSubTitle();
   }
 
   @Override
@@ -71,9 +79,22 @@ public class CrimeListFragment extends Fragment {
   }
 
   @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putBoolean(SAVED_SUBTITLE_VISIBLE, mSubtitleVisible);
+  }
+
+  @Override
   public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
     super.onCreateOptionsMenu(menu, inflater);
     inflater.inflate(R.menu.fragment_crime_list, menu);
+
+    MenuItem subtitleItem = menu.findItem(R.id.show_subtitle);
+    if (mSubtitleVisible) {
+      subtitleItem.setTitle(R.string.hide_subtitle);
+    } else {
+      subtitleItem.setTitle(R.string.show_subtitle);
+    }
   }
 
   @Override
@@ -85,9 +106,27 @@ public class CrimeListFragment extends Fragment {
         Intent intent = CrimePagerActivity.newIntent(getActivity(), crime.getId());
         startActivity(intent);
         return true;
+      case R.id.show_subtitle:
+        mSubtitleVisible = !mSubtitleVisible;
+        getActivity().invalidateOptionsMenu();
+        updateSubTitle();
+        return true;
       default:
         return super.onOptionsItemSelected(item);
     }
+  }
+
+  private void updateSubTitle() {
+    CrimeLab crimeLab = CrimeLab.get(getActivity());
+    int crimeCount = crimeLab.getCrimes().size();
+    String subtitle = String.format(getString(R.string.subtitle_format), crimeCount);
+
+    if (!mSubtitleVisible) {
+      subtitle = null;
+    }
+
+    AppCompatActivity activity = (AppCompatActivity) getActivity();
+    activity.getSupportActionBar().setSubtitle(subtitle);
   }
 
   private class CrimeHolder extends RecyclerView.ViewHolder {
